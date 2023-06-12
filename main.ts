bfbe174e65f3ce7bbe5842ddad4b091bfb7f7d34
val0817 <131650612+val0817@users.noreply.github.com>
2023-06-12 09:15:27 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+})
+basic.forever(function () {
+	
+})
