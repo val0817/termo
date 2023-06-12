480b7d86a8fb383bd0a146b61d458f034334a18b
val0817 <131650612+val0817@users.noreply.github.com>
2023-06-12 09:55:25 +0200

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(input.temperature() + 273)
-})
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.temperature() * 9)
 })
